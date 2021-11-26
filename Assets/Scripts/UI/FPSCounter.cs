59cdf7185f3b691bd341afd162a3829599c56f62
maxim <max20042011@gmail.com>
2021-11-26 17:04:55 +0300

    moved to urp

---
+++ Assets/Scripts/UI/FPSCounter.cs
@@ -0,0 +1,16 @@
+using UnityEngine.UI;
+using UnityEngine;
+
+public class FPSCounter : MonoBehaviour
+{
+    public int avgFrameRate;
+    public Text display_Text;
+    public void Update ()
+        {
+            float current = 0;
+            current = (int)(1f / Time.unscaledDeltaTime);
+            avgFrameRate = (int)current;
+            display_Text.text = avgFrameRate.ToString() + " FPS";
+        }
+}
+
---
+++ Assets/Scripts/UI/Minimap/MinimapClampMarker.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapClampMarker : MonoBehaviour {
+
+	public Transform MinimapCam;
+	public float MinimapSize;
+	Vector3 TempV3;
+
+	void Update () 
+        {
+            TempV3 = transform.parent.transform.position;
+            TempV3.y = transform.position.y;
+            transform.position = TempV3;
+            transform.position = new Vector3 (
+                Mathf.Clamp(transform.position.x, MinimapCam.position.x-MinimapSize, MinimapSize+MinimapCam.position.x),
+                transform.position.y,
+                Mathf.Clamp(transform.position.z, MinimapCam.position.z-MinimapSize, MinimapSize+MinimapCam.position.z)
+            );
+        }
+
+	
+}
